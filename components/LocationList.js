585faa14f270578a4e3451356524aa3c96983e81
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-09 00:53:17 -0400

    component locationlist

---
+++ components/LocationList.js
@@ -0,0 +1,35 @@
+const react = require('react');
+const LocationItem = require('./LocationItem');
+
+const LocationList = React.createClass({
+
+        render() {
+
+              let self = this;
+
+              let locations = this.props.locations.map(function(location) {
+
+                    let active = self.props.activeLocationAddress == location.address;
+
+                    //notice that we are passing the onClick callback of this
+                    //LocationList to each LocationItem.
+
+                    return <LocationItem address = { location.address} timestamp={location.timestamp}
+                          active={active} onClick={self.props.onClick} />
+              });
+
+              if (!locations.length) {
+                    return null;
+              }
+
+              return {
+
+                    <div className="list-group col-xs-12 col-md-6 col-md-offset-3">
+                          <span className="list-group-item active">Saved Locations</span>
+                          {locations}
+                    </div>
+              }
+        }
+});
+
+module.exports = LocationList; 
