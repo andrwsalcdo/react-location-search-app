23a95091d9f1b876f19d5aca082f66e5661a480f
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-09 01:00:06 -0400

    component location item

---
+++ components/LocationItem.js
@@ -0,0 +1,29 @@
+const react = require('react');
+const LocationItem = require('./LocationItem');
+const moment = require('moment');
+
+const LocationItem = React.createClass({
+
+        handleClick() {
+              this.props.onClick(this.props.address);
+        },
+
+        render() {
+
+              let cn = "list-group-item";
+
+              if(this.props.active) {
+                    cn += "active-location";
+              }
+              return(
+
+                    <a className={cn} onClick={this.handleClick}>
+                          {this.props.address}
+                          <span className="createdAt">{ moment(this.props.timestamp).fromNow() }</span>
+                          <span className="glyphicon glyphicon-menu-right"></span>
+                    </a>
+              )
+        }
+});
+
+module.exports = LocationItem; 
